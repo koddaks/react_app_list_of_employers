037244f4ba9af5e3b2a99c1f8c5eccf5d301ce37
koddaks <koss199710@gmail.com>
2023-02-23 10:47:53 +0200

    modified

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,25 +1,15 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/app/app';
 
-const text = 'Hello world';
-const date = new Date();
-
-const elem = (
-  <div>
-    <h2 className='greetings'>{text}</h2>
-    <input type='text'/>
-    <label htmlFor=""></label>
-    <button tabIndex='0'>click</button>       
-  </div>
-);
-
-// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello world');
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem,
+  <StrictMode>
+    <App/>
+  </StrictMode>
+  
 );
 
