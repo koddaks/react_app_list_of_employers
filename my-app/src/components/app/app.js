5080ec333f98b4578b64b92e8c0c0168bcc17d5e
koddaks <koss199710@gmail.com>
2023-04-12 14:31:06 +0300

    make Search Employers

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -1,22 +1,23 @@
-import { Component } from "react";
+import { Component } from 'react';
 
-import AppInfo from "../app-info/app-info";
-import SearchPanel from "../search-panel/search-panel";
-import AppFilter from "../app-filter/app-filter";
-import EmployersList from "../employers-list/employers-list";
-import EmployersAddForm from "../employers-add-form/employers-add-form";
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
 
-import "./app.css";
+import './app.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [
-        { name: "John C.", salary: 800, increase: false, rise: true, id: 1 },
-        { name: "Alex M.", salary: 3000, increase: false, rise: false, id: 2 },
-        { name: "Carl W.", salary: 25000, increase: true, rise: false, id: 3 },
+        { name: 'John C.', salary: 800, increase: false, rise: true, id: 1 },
+        { name: 'Alex M.', salary: 3000, increase: false, rise: false, id: 2 },
+        { name: 'Carl W.', salary: 25000, increase: true, rise: false, id: 3 },
       ],
+      term: '',
     };
     this.maxId = 4;
   }
@@ -31,7 +32,7 @@ class App extends Component {
     });
   };
 
-  onToggleProp = (id, prop) => {  
+  onToggleProp = (id, prop) => {
     this.setState(({ data }) => ({
       data: data.map((item) => {
         if (item.id === id) {
@@ -41,8 +42,6 @@ class App extends Component {
       }),
     }));
   };
-
-  
 
   addItem = (name, salary) => {
     const newItem = {
@@ -61,27 +60,39 @@ class App extends Component {
     });
   };
 
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+
+    return items.filter((item) => {
+      return item.name.indexOf(term) > -1;
+    });
+  };
+
+  onUpdateSearch = (term) => {
+    this.setState({term})
+  }
+
   render() {
-    const { data } = this.state;
+    const { data, term } = this.state;
     const emlpoyers = data.length;
     const increased = data.filter((item) => item.increase).length;
+    const visibleData = this.searchEmp(data, term);
 
     return (
       <div className="app">
-        <AppInfo 
-        emlpoyers={emlpoyers} 
-        increased={increased} />
+        <AppInfo emlpoyers={emlpoyers} increased={increased} />
 
         <div className="search-panel">
-          <SearchPanel />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
           <AppFilter />
         </div>
 
         <EmployersList
-          data={data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp}
-          
         />
         <EmployersAddForm onAdd={this.addItem} />
       </div>
--- my-app/src/components/search-panel/search-panel.js
+++ my-app/src/components/search-panel/search-panel.js
@@ -1,12 +1,31 @@
+import { Component } from 'react';
 import './search-panel.css';
 
-const SearchPanel = () => {
+class SearchPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+    };
+  }
+
+  onUpdateSearch = (e) => {
+    const term = e.target.value;
+    this.setState({term});
+    this.props.onUpdateSearch(term)
+  }
+
+  render() {
     return (
-        <input 
+      <input
         type="text"
-        className='form-control search-input'
-        placeholder='Найти  сотудника' />
-    )
+        className="form-control search-input"
+        placeholder="Найти  сотудника"
+        value={this.state.term}
+        onChange={this.onUpdateSearch}
+      />
+    );
+  }
 }
 
 export default SearchPanel;
